c0154ce58032f82a97f6af9791ad0104874b54b5
Jackson Fleege <148395567+jfleege@users.noreply.github.com>
2025-07-10 11:47:01 -0500

    Add files via upload
    added a few functions; including mark card function

--- stat385_finalproject_bingo.R
+++ stat385_finalproject_bingo.R
@@ -9,7 +9,7 @@ number_pool <- c(1:100)
 # samples the number pool with 25 numbers, making the players card
 rough_player_card <- sample(number_pool, 25)
 
-# makes the card fancier
+# makes the card fancier for aesthetic 
 card_banner <- list(c("B", "I", "N", "G", "O"), c("B", "I", "N", "G", "O"))
 
 # builds the players card with the sample from the number pool
@@ -20,3 +20,21 @@ temp_player_card[3,3] <- "FREE"
 player_card <- as.data.frame(temp_player_card)
 
 #-----------------------------------------------------------#
+
+# draws numbers that will be used to cross check with the players card
+number_called <- number_pool
+
+# creates a function that removes a number from the card if matched
+called_number <- function() {
+  sample(number_called, 1, replace = FALSE)
+  number_called <<- number_called[number_called != called_number]
+  return(called_number)
+}
+
+# checks the number against the players card and if matched marks with an X
+mark_card <- function(card, number) {
+  card[card == number] <- "X"
+  return(card)
+}
+
+
